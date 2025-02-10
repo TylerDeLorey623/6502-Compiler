bbc47409d3b3b653df152674a47590d59c3ec4b3
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2025-02-09 23:49:43 -0500

    Added splitPrograms function to split each program by the $ delimiter

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <vector>
 #include <regex>
 
@@ -14,17 +15,20 @@ class Lexer
     public:
         Lexer(const string inputCode)
         {
-            this->code = inputCode;
+            this->program = inputCode;
         }
 
+        // Tokenize the program to be used by the Parser
         vector<Token> tokenize()
         {
+            vector<Token> tokens;
 
+            return tokens;
         }
 
     private:
         // Holds all of the code in the file
-        string code;
+        string program;
 
         // Regular expressions for this entire grammar
         regex keywordREGEX = regex("\\b(print|while|if|int|string|boolean|true|false)\\b");
@@ -34,11 +38,29 @@ class Lexer
         regex charREGEX = regex("[a-z]");
 };
 
+// Splits up the programs by the delimiter
+vector<string> splitPrograms(const string input, const char delimiter)
+{
+    vector<string> segments;
+    stringstream stream(input);
+    string str;
+
+    while (getline(stream, str, delimiter))
+    {
+        segments.push_back(str);
+    }
+
+    // Test if last character is the delimiter for warning error
+    if (input.back() != delimiter)
+    {
+        cout << "WARNING: The final program does not end with a '" << delimiter << "'." << endl;
+    }
+
+    return segments;
+}
+
 int main() 
 {
-    // Variable declaration
-    
-    
     // File IO
     ifstream file("code.txt");
     if (!file)
@@ -49,6 +71,20 @@ int main()
 
     // Copies all characters from the file to the code string
     string code((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+
+    // Create the separate programs separated with $
+    vector<string> programs = splitPrograms(code, '$');
+
+    // Compile each program
+    for (int i = 0, size = programs.size(); i < size; i++)
+    {
+        Lexer* currentLex = new Lexer(programs[i]);
+        vector<Token> tokens = currentLex->tokenize();
+        // PARSER
+        // SEMANATIC ANALYSIS
+        // CODE GEN
+        delete(currentLex);
+    }
 
     //cout << "DEBUG Lexer - " << savedItem << " [ " <<  savedBuffer << " ] found at (" << savedItemLine << ":" << savedItemColumn << ")" << endl;
 
