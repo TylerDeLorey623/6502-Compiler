ce54e97951c3490e53eebb80893a283e316f3682
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2025-02-09 23:02:57 -0500

    Beginning process to redo how Lexer performs

--- main.cpp
+++ main.cpp
@@ -4,55 +4,27 @@
 
 #include "Token.h"
 #include "Symbol.h"
+#include "Keyword.h"
 
 using namespace std;
 
 int main() 
 {
     // Variable declaration
-    char ch;
-    string buffer = "";
-    int lastPosition = -1;
-    int currentPosition = 0;
-
-    int currentSymbolLine = 1;
-    int currentSymbolColumn = 1;
-    int savedSymbolLine = 1;
-    int savedSymbolColumn = 1;
+    
     
     // File IO
-    ifstream file;
-    vector<char> lines;
-
-    // Open file
-    file.open("code.txt");
-    if (!file.is_open())
+    ifstream file("code.txt");
+    if (!file)
     {
         cout << "File failed to open." << endl;
         return 1;
     }
 
-    while (file.get(ch))
-    {
-        buffer += ch;
+    // Copies all characters from the file to the code string
+    string code((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
 
-        if (ch == '\n')
-        {
-            currentSymbolLine++; 
-            currentSymbolColumn = 0;   
-        }
-
-        Symbol sym = getSymbol(ch);
-        if (sym != Symbol::UNKNOWN)
-        {
-            savedSymbolLine = currentSymbolLine;
-            savedSymbolColumn = currentSymbolColumn;
-            cout << "DEBUG Lexer - " << symbolToString(sym) << " [ " <<  ch << " ] found at (" << savedSymbolLine << ":" << savedSymbolColumn << ")" << endl;
-        }
-
-        currentSymbolColumn++;
-        currentPosition++;
-    }
+    //cout << "DEBUG Lexer - " << savedItem << " [ " <<  savedBuffer << " ] found at (" << savedItemLine << ":" << savedItemColumn << ")" << endl;
 
     // Close file
     file.close();
