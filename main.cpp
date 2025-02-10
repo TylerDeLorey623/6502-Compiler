1bfde6d297c073973c89a91446368d98af89d60a
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2025-02-09 23:21:19 -0500

    Added Lexer class with beginnings of RegEX workings

--- main.cpp
+++ main.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <regex>
 
 #include "Token.h"
 #include "Symbol.h"
 #include "Keyword.h"
 
 using namespace std;
+
+class Lexer 
+{
+    public:
+        Lexer(const string inputCode)
+        {
+            this->code = inputCode;
+        }
+
+        vector<Token> tokenize()
+        {
+
+        }
+
+    private:
+        // Holds all of the code in the file
+        string code;
+
+        // Regular expressions for this entire grammar
+        regex keywordREGEX = regex("\\b(print|while|if|int|string|boolean|true|false)\\b");
+        regex idREGEX = regex("[a-z]+");
+        regex symbolREGEX = regex(R"(\{|\}|"|\(|\)|==|!=|\+|=|\s|\$)");
+        regex digitREGEX = regex("[0-9]");
+        regex charREGEX = regex("[a-z]");
+};
 
 int main() 
 {
