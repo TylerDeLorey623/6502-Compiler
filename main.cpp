c6dca7dbf3dbf5e55a4f74d1c4843c6c924db55d
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2025-02-10 20:49:46 -0500

    Added arguments to main function, passed in with make

--- main.cpp
+++ main.cpp
@@ -5,108 +5,9 @@
 #include <regex>
 
 #include "Token.h"
+#include "Lexer.h"
 
 using namespace std;
-
-class Lexer 
-{
-    public:
-        // Default constructor for the Lexer class
-        Lexer(const string inputCode)
-        {
-            this->program = inputCode;
-        }
-
-        // Tokenize the program to be used by the Parser
-        pair<vector<Token>, int> tokenize()
-        {
-            vector<Token> tokens;
-            smatch match;
-
-            int size = program.size();
-            while (currentPosition < size)
-            {
-                string programSnippet = program.substr(currentPosition);
-
-                // Ignores all comments
-                if (regex_search(programSnippet, match, commentREGEX))
-                {
-                    currentPosition += match.length(0);
-                    continue;
-                }
-                // Ignores whitespace
-                else if (!inQuotes && regex_search(programSnippet, match, spaceREGEX))
-                {
-                    currentPosition += match.length(0);
-                    continue;
-                }
-
-                // Detects keywords
-                if (regex_search(programSnippet, match, keywordREGEX))
-                {
-                    cout << "KEYWORD " << match.str(0) << endl;
-                    currentPosition += match.length(0);
-                }
-                // Detects IDs
-                else if (regex_search(programSnippet, match, idREGEX))
-                {
-                    cout << "ID " << match.str(0) << endl;
-                    currentPosition += match.length(0);
-                    
-                    requireID = false;
-                }
-                // Detects symbols
-                else if (regex_search(programSnippet, match, symbolREGEX))
-                {
-                    cout << "SYMBOL " << match.str(0) << endl;
-                    currentPosition += match.length(0);
-                }
-                // Detects digits
-                else if (regex_search(programSnippet, match, digitREGEX))
-                {
-                    cout << "DIGIT " << match.str(0) << endl;
-                    currentPosition += match.length(0);
-                }
-                // Detects chars
-                else if (inQuotes && regex_search(programSnippet, match, charREGEX))
-                {
-                    cout << "CHAR " << match.str(0) << endl;
-                    currentPosition += match.length(0);
-                }
-                else
-                {
-                    cout << "Unrecognized Token: " << programSnippet[0] << endl;
-                    errorCount++;
-                    break;
-                }
-
-            }
-
-
-            return make_pair(tokens, errorCount);
-        }
-
-    private:
-        // Holds all of the code in the file
-        string program;
-
-        int currentPosition = 0;
-        bool inQuotes = false;
-
-        string storedKeyword;
-        bool requireID = false;
-
-        int errorCount = 0;
-
-        // Regular expressions for this entire grammar
-        regex commentREGEX = regex(R"(^\/\*.*?\*\/)");
-        regex spaceREGEX = regex(R"(^\s+)");
-        regex keywordREGEX = regex(R"(^\b(print|while|if|int|string|boolean|true|false)\b)");
-        regex idREGEX = regex(R"(^[a-z]+)");
-        regex symbolREGEX = regex(R"(^(\{|\}|"|\(|\)|==|!=|\+|=|\$))");
-        regex digitREGEX = regex(R"(^[0-9])");
-        regex charREGEX = regex(R"(^[a-zA-Z])");
-};
 
 // Splits up the programs by the delimiter
 vector<string> splitPrograms(const string input, const char delimiter)
@@ -117,7 +18,7 @@ vector<string> splitPrograms(const string input, const char delimiter)
 
     while (getline(stream, str, delimiter))
     {
-        segments.push_back(str);
+        segments.emplace_back(str);
     }
 
     // Test if last character is the delimiter for warning error
@@ -129,10 +30,17 @@ vector<string> splitPrograms(const string input, const char delimiter)
     return segments;
 }
 
-int main() 
+
+int main(int argc, char* argv[]) 
 {
     // File IO
-    ifstream file("code.txt");
+    if (argc != 2)
+    {
+        cout << "No arguments given. Use command 'make ARGS='filename''." << endl;
+        return 1;
+    }
+
+    ifstream file(argv[1]);
     if (!file)
     {
         cout << "File failed to open." << endl;
