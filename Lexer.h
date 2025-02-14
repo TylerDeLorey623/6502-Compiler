5bccf756f6aa1c36bfd44253c7e995d28fdcf7a5
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2025-02-13 23:55:07 -0500

    Added more error and warning messages dealing with quotes, comments, and the delimiter

--- Lexer.h
+++ Lexer.h
@@ -12,13 +12,14 @@ class Lexer
 {
     public:
         // Default constructor for the Lexer class
-        Lexer(const string inputCode)
+        Lexer(const string inputCode, const char del)
         {
             this->program = inputCode;
+            this->delimiter = del;
         }
 
         // Tokenize the program to be used by the Parser
-        pair<vector<Token>, int> tokenize()
+        pair<vector<Token>, pair<int, int> > tokenize()
         {
             vector<Token> tokens;
             smatch match;
@@ -34,6 +35,33 @@ class Lexer
                 {
                     currentPosition += match.length(0);
                     LINECOLUMN += match.length(0);
+                    continue;
+                }
+                // Detect if comment opens and doesn't close
+                else if (regex_search(programSnippet, match, commentBeginREGEX))
+                {
+                    cout << "ERROR Lexer - Unclosed Comment starting at (" << LINEROW << ":" << LINECOLUMN << ")" << endl;
+                    errorCount++;
+                    currentPosition += match.length(0);
+                    LINECOLUMN += match.length(0);
+                    openComment = true;
+                    continue;
+                }
+                // Detect if comment closed but it never opened
+                else if (regex_search(programSnippet, match, commentEndREGEX))
+                {
+                    cout << "ERROR Lexer - Unpaired */ token at (" << LINEROW << ":" << LINECOLUMN << ")" << endl;
+                    errorCount++;
+                    currentPosition += match.length(0);
+                    LINECOLUMN += match.length(0);
+                    continue;
+                }
+
+                // Open comments leave no tokens, so skip iteration
+                if (openComment)
+                {
+                    currentPosition++;
+                    LINECOLUMN++;
                     continue;
                 }
 
@@ -61,21 +89,32 @@ class Lexer
                         {
                             inQuotes = true;
                         }
+
+                        // For unclosed strings
                         lastQuoteRow = LINEROW;
                         lastQuoteCol = LINECOLUMN;
                     }
                     // If code moves on to a new line, adjust line row/column values
                     else if (match.str(0) == "\n" || match.str(0) == "\r" || match.str(0) == "\r\n")
                     {
+                        // If still in quotes at the end of a line, there is an unclosed string
+                        if (inQuotes)
+                        {
+                            cout << "ERROR Lexer - Unclosed string starting at (" << lastQuoteRow << ":" << lastQuoteCol << ")" << endl;
+                            errorCount++;
+                            inQuotes = false;
+                        }
+
                         LINECOLUMN = 1;
                         LINEROW++;
                         currentPosition += match.length(0);
+                        
                         continue;
                     }
                     // Symbols cannot appear in quotes, so throw error
                     else if (inQuotes)
                     {
-                        cout << "ERROR Lexer - Error:" << LINEROW << ":" << LINECOLUMN << " Unrecognized Token: " << programSnippet[0] << endl;
+                        cout << "ERROR Lexer - Unrecognized Token [ " << programSnippet[0] << " ] at (" << LINEROW << ":" << LINECOLUMN << ")" << endl;
                         errorCount++;
                         currentPosition++;
                         LINECOLUMN++;
@@ -120,32 +159,42 @@ class Lexer
                 // If there were no matches, there was an unrecognized token
                 else
                 {
-                    cout << "ERROR Lexer - Error:" << LINEROW << ":" << LINECOLUMN << " Unrecognized Token: " << programSnippet[0] << endl;
+                    cout << "ERROR Lexer - Unrecognized Token [ " << programSnippet[0] << " ] at (" << LINEROW << ":" << LINECOLUMN << ")" << endl;
                     errorCount++;
                     LINECOLUMN++;
                     currentPosition++;
                 }
             }
 
-            // If still in quotes at the end, there is an unclosed string
+            // If still in quotes at the end of the program, there is an unclosed string
             if (inQuotes)
             {
                 cout << "ERROR Lexer - Error: Unclosed string starting at (" << lastQuoteRow << ":" << lastQuoteCol << ")" << endl;
                 errorCount++;
             }
 
-            return make_pair(tokens, errorCount);
+            // Test if last character is the delimiter for warning error
+            if (program.back() != delimiter || openComment)
+            {
+                cout << "WARNING Lexer: The final program didn't end with a '$'" << endl;
+                warningCount++;
+            }
+
+            return make_pair(tokens, make_pair(errorCount, warningCount));
         }
 
     private:
         // Holds all of the code in the file
         string program;
+        char delimiter;
 
         int currentPosition = 0;
         bool inQuotes = false;
+        bool openComment = false;
 
-        // For total error count
+        // For total error/warning count
         int errorCount = 0;
+        int warningCount = 0;
         
         // Determines positions of unclosed strings
         int lastQuoteRow = 0;
@@ -153,6 +202,9 @@ class Lexer
 
         // Regular expressions for this entire grammar
         const regex commentREGEX = regex(R"(^\/\*.*?\*\/)");
+        const regex commentBeginREGEX = regex(R"(^\/\*)");
+        const regex commentEndREGEX = regex(R"(^\*\/)");
+
         const regex spaceREGEX = regex(R"(^[ \t])");
         const regex keywordREGEX = regex("^(print|while|if|int|string|boolean|true|false)");
         const regex idREGEX = regex(R"(^[a-z])");
@@ -167,9 +219,9 @@ class Lexer
             {"print", "PRINT_STATEMENT"},
             {"while", "WHILE_STATEMENT"},
             {"if", "IF_STATEMENT"},
-            {"int", "VARIABLE_TYPE"},
-            {"string", "VARIABLE_TYPE"},
-            {"boolean", "VARIABLE_TYPE"},
+            {"int", "I_VARTYPE"},
+            {"string", "S_VARTYPE"},
+            {"boolean", "B_VARTYPE"},
             {"true", "BOOL_VAL"},
             {"false", "BOOL_VAL"},
 
