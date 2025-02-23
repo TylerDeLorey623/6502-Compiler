7c81aaa55d87ed2bf8cc2e080a5ef6e0de2ae6dd
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2025-02-22 21:33:33 -0500

    Implemented the basic functionality for the parser (very unstable)

--- Parser.h
+++ Parser.h
@@ -8,21 +8,248 @@ class Parser
 {
     public:
         // Default constructor for the Parser class
-        Parser(const int progNum, const vector<Token> lexTokens)
+        Parser(const int progNum, const vector<Token> lexTokens, const char del)
         {
             this->programNumber = progNum;
             this->tokens = lexTokens; 
+            this->delimiter = del;
+
+            this->currentToken = tokens[currentIndex]; 
+            this->currentTokenType = currentToken.type;
         }
 
         // Validates the tokens
-        void validate()
+        void parse()
         {
-            
+            parseProgram();
         }
 
     private:
+        // Default members
         int programNumber;
         vector<Token> tokens;
+        string delimiter;
+
+        // Current token, its type, and index in the tokens vector
+        Token currentToken;
+        string currentTokenType;
+        int currentIndex = 0;
+
+        int errorCount = 0;
+        int warningCount = 0;
+
+        void match(string expectedTokenType)
+        {
+            if (currentToken.type == expectedTokenType)
+            {
+                // CONSUME
+                currentIndex++;
+                currentToken = tokens[currentIndex];
+                currentTokenType = currentToken.type;
+            }
+            else
+            { 
+                cout << "EXPECTED " << expectedTokenType << " BUT FOUND " << currentToken.type << endl;
+                currentIndex++;
+                currentToken = tokens[currentIndex];
+                currentTokenType = currentToken.type;
+            }
+        }
+
+        void parseProgram()
+        {
+            parseBlock();
+            match("EOP");
+        }
+
+        void parseBlock()
+        {
+            match("OPEN_CURLY");
+            parseStatementList();
+            match("CLOSE_CURLY");
+        }
+
+        void parseStatementList()
+        {
+            if (currentTokenType == "PRINT_STATEMENT" || currentTokenType == "ID" || 
+                currentTokenType == "I_VARTYPE" || currentTokenType == "S_VARTYPE" || currentTokenType == "B_VARTYPE" ||
+                currentTokenType == "WHILE_STATEMENT" || currentTokenType == "IF_STATEMENT" || currentTokenType == "OPEN_CURLY")
+            {
+                parseStatement();
+                parseStatementList();
+            }
+            else
+            {
+                // nothing
+                // its an ε
+                // production
+            }
+        }
+
+        void parseStatement()
+        {
+            if (currentTokenType == "PRINT_STATEMENT")
+            {
+                parsePrintStatement();
+            }
+            else if (currentTokenType == "ID")
+            {
+                parseAssignmentStatement();
+            }
+            else if (currentTokenType == "I_VARTYPE" || currentTokenType == "S_VARTYPE" || currentTokenType == "B_VARTYPE")
+            {
+                parseVarDecl();
+            }
+            else if (currentTokenType == "WHILE_STATEMENT")
+            {
+                parseWhileStatement();
+            }
+            else if (currentTokenType == "IF_STATEMENT")
+            {
+                parseIfStatement();
+            }
+            // Will give error if the type isn't a open curly brace
+            else
+            {
+                parseBlock();
+            }
+        }
+
+        void parsePrintStatement()
+        {
+            match("PRINT_STATEMENT");
+            match("OPEN_PARENTHESIS");
+            parseExpr();
+            match("CLOSE_PARENTHESIS");
+        }
+
+        void parseAssignmentStatement()
+        {
+            parseId();
+            match("ASSIGNMENT_OP");
+            parseExpr();
+        }
+
+        void parseVarDecl()
+        {
+            if (currentToken.type == "I_VARTYPE")
+            {
+                match("I_VARTYPE");
+                parseId();
+            }
+            else if (currentToken.type == "S_VARTYPE")
+            {
+                match("S_VARTYPE");
+                parseId();
+            }
+            // Will give error if type isn't a boolean variable
+            else
+            {
+                match("B_VARTYPE");
+                parseId();
+            }
+        }
+
+        void parseWhileStatement()
+        {
+            match("WHILE_STATEMENT");
+            parseBooleanExpr();
+            parseBlock();
+        }
+
+        void parseIfStatement()
+        {
+            match("IF_STATEMENT");
+            parseBooleanExpr();
+            parseBlock();
+        }
+
+        void parseExpr()
+        {
+            if (currentTokenType == "DIGIT")
+            {
+                parseIntExpr();
+            }
+            else if (currentTokenType == "QUOTE")
+            {
+                parseStringExpr();
+            }
+            else if (currentTokenType == "OPEN_PARENTHESIS" || currentTokenType == "BOOL_VAL")
+            {
+                parseBooleanExpr();
+            }
+            // Will give error if the type isn't an ID
+            else
+            {
+                parseId();
+            }
+        }
+
+        void parseIntExpr()
+        {
+            match("DIGIT");
+            if (currentToken.type == "ADDITION_OP")
+            {
+                match("ADDITION_OP");
+                parseExpr();
+            }
+        }
+
+        void parseStringExpr()
+        {
+            match("QUOTE");
+            parseCharList();
+            match("QUOTE");
+        }
+
+        void parseBooleanExpr()
+        {
+            if (currentToken.type == "OPEN_PARENTHESIS")
+            {
+                match("OPEN_PARENTHESIS");
+                parseExpr();
+                if (currentToken.type == "EQUALITY_OP")
+                {
+                    match("EQUALITY_OP");
+                }
+                // Will give error if type isn't an inequality operator
+                else
+                {
+                    match("INEQUALITY_OP");
+                }
+                parseExpr();
+            }
+            // Will give error if type isn't a boolean value
+            else
+            {
+                match("BOOL_VAL");
+            }
+        }
+
+        void parseId()
+        {
+            match("CHAR");
+        }
+
+        void parseCharList()
+        {
+            if (currentToken.type == "CHAR")
+            {
+                match("CHAR");
+                parseCharList();
+            }
+            else if (currentToken.type == "SPACE")
+            {
+                match("SPACE");
+                parseCharList();
+            }
+            else
+            {
+                // nothing
+                // its an ε
+                // production
+            }
+        }
 };
 
 #endif
