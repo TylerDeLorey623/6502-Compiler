013f2d9d807cba3634c1a8b5bfcf26b954110c0b
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2025-02-22 21:32:51 -0500

    Added empty constructor for Parser placeholder

--- Token.h
+++ Token.h
@@ -13,12 +13,22 @@ class Token
         int line;
         int column;
 
+        // Parameterized Constructor
         Token(string newType, string newLexeme, int newLine, int newColumn)
         {
             this->type = newType;
             this->lexeme = newLexeme;
             this->line = line;
             this->column = newColumn;
+        }
+
+        // Empty constructor (default in Parser class, gets rewritten)
+        Token()
+        {
+            this->type = "UNKNOWN";
+            this->lexeme = "UNKNOWN";
+            this->line = 0;
+            this->column = 0;
         }
 
 };
